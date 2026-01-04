6fa520d10f521eb90980bf12bb9626331c7436f0
Enric <164487968+spocker-1@users.noreply.github.com>
2026-01-04 21:33:24 +0100

    Update const.py

--- custom_components/spock_ems_growatt/const.py
+++ custom_components/spock_ems_growatt/const.py
@@ -1,5 +1,8 @@
+"""Constantes para la integración Growatt Spock EMS."""
+
 DOMAIN = "growatt_spock_ems"
 
+# Config Keys
 CONF_SPOCK_ID = "spock_id"
 CONF_SPOCK_API_TOKEN = "spock_api_token"
 CONF_SPOCK_PLANT_ID = "spock_plant_id"
@@ -7,8 +10,12 @@ CONF_INVERTER_IP = "inverter_ip"
 CONF_MODBUS_PORT = "modbus_port"
 CONF_MODBUS_ID = "modbus_id"
 
+# Defaults
 DEFAULT_PORT = 502
 DEFAULT_MODBUS_ID = 1
 
-# Endpoint siguiendo la lógica de SMA
+# Endpoint
 SPOCK_TELEMETRY_API_ENDPOINT = "https://ems-ha.spock.es/api/ems_growatt"
+
+# Plataformas soportadas
+PLATFORMS = ["sensor"]
